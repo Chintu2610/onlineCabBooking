196824b4e48831fa76447243f3dac3c22e47c724
Chintu2610 <byamakeshpalei@gmail.com>
2024-03-15 11:59:28 +0530

    initial commit

---
+++ src/main/java/com/cab/Model/User.java
@@ -0,0 +1,23 @@
+package com.cab.Model;
+
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@MappedSuperclass
+public abstract class User {
+
+	private String userName;
+	private String password;
+	private String address;
+	private String mobileNumber;
+	private String email;
+	private String userRole;
+	
+}
