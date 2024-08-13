6bc6257a6f0914306e78584cf14810fa94f40b06
Sharunay1 <sharathnayak1026@gmail.com>
2024-08-13 18:58:31 +0530

    add trip booking model
    add trip booking model

--- src/main/java/com/cab/Model/TripBooking.java
+++ src/main/java/com/cab/Model/TripBooking.java
@@ -51,4 +51,93 @@ public class TripBooking {
 	@JsonIgnore
 	private Cab cab;
 
+	
+	public Integer getTripBookingId() {
+		return tripBookingId;
+	}
+
+	public void setTripBookingId(Integer tripBookingId) {
+		this.tripBookingId = tripBookingId;
+	}
+
+	public String getPickupLocation() {
+		return pickupLocation;
+	}
+
+	public void setPickupLocation(String pickupLocation) {
+		this.pickupLocation = pickupLocation;
+	}
+
+	public String getFromDateTime() {
+		return fromDateTime;
+	}
+
+	public void setFromDateTime(String fromDateTime) {
+		this.fromDateTime = fromDateTime;
+	}
+
+	public String getDropLocation() {
+		return dropLocation;
+	}
+
+	public void setDropLocation(String dropLocation) {
+		this.dropLocation = dropLocation;
+	}
+
+	public String getToDateTime() {
+		return toDateTime;
+	}
+
+	public void setToDateTime(String toDateTime) {
+		this.toDateTime = toDateTime;
+	}
+
+	public String getPrice() {
+		return price;
+	}
+
+	public void setPrice(String price) {
+		this.price = price;
+	}
+
+	public float getDistanceInKm() {
+		return distanceInKm;
+	}
+
+	public void setDistanceInKm(float distanceInKm) {
+		this.distanceInKm = distanceInKm;
+	}
+
+	public String getCurrStatus() {
+		return currStatus;
+	}
+
+	public void setCurrStatus(String currStatus) {
+		this.currStatus = currStatus;
+	}
+
+	public Driver getDriver() {
+		return driver;
+	}
+
+	public void setDriver(Driver driver) {
+		this.driver = driver;
+	}
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+
+	public Cab getCab() {
+		return cab;
+	}
+
+	public void setCab(Cab cab) {
+		this.cab = cab;
+	}
+
 }
